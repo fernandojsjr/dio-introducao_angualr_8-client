b67ae393ec05d9202183ac3543550b45c8769632
Fernando J. Silva <fernandojsjr@gmail.com>
2022-04-27 22:22:54 -0300

    criando componentes e construindo o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { CourseListComponent } from './courses/course-list.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
