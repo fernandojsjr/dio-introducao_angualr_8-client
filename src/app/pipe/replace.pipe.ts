25b4393af274f5e15611adaa7db94d14300f99a5
Fernando J. Silva <fernandojsjr@gmail.com>
2022-04-29 23:04:45 -0300

    transformando datas com Pipes

---
+++ src/app/pipe/replace.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe } from "@angular/core";
+
+@Pipe({
+  name: 'replace'
+})
+export class ReplacePipe {
+
+  transform(value: string, char: string, valueToReplace: string){
+    return value.replace(char, valueToReplace); 
+  }
+
+}
