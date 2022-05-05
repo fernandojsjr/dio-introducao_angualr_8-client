eff81d7d87b44999e8f4b5d217b10369e8668eea
Fernando J. Silva <fernandojsjr@gmail.com>
2022-05-04 23:10:26 -0300

    conhecendo a pasta core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './404/error-404.component';
 import { CourseModule } from './courses/course.module'
+import { CoreModule } from './core/component/nav-bar/core.module';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
